97db38006468bea447d8e7aa271a04df10b2766d
Senya <senya@riseup.net>
2020-03-15 23:46:22 +0200

    Bad certificate error handling

--- lib/yadisk.rb
+++ lib/yadisk.rb
@@ -5,16 +5,15 @@ require 'cgi/util'
 require 'uri'
 require 'io/console'
 require 'json'
+require 'net/http'
 
 module Yadisk
   class Main
     BASE_URL = 'https://cloud-api.yandex.net:443/v1/disk/public/resources/download?public_key='
     def download(url, folder: './')
       enc_url = CGI::escape(url)
-
-      puts "wget -qO - #{BASE_URL}#{enc_url}"
-      res = IO.popen("wget -qO - #{BASE_URL}#{enc_url}").read
-      json_res = JSON.parse(res)
+      response = Net::HTTP.get(URI("#{BASE_URL}#{enc_url}"))
+      json_res = JSON.parse(response)
       download_url = json_res['href']
       filename = CGI::parse(URI(download_url).query)["filename"][0]
       folder = folder + "/" if not folder.end_with?('/')
