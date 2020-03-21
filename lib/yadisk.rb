0e4868df82c6257698f9fd3578f1335c79da25e2
Senya <senya@riseup.net>
2020-03-22 01:44:00 +0200

    Support passing custom options to wget

--- lib/yadisk.rb
+++ lib/yadisk.rb
@@ -10,7 +10,7 @@ require 'net/http'
 module Yadisk
   class Main
     BASE_URL = 'https://cloud-api.yandex.net:443/v1/disk/public/resources/download?public_key='
-    def download(url, folder: './')
+    def download(url, folder: './', wget_options: nil)
       enc_url = CGI::escape(url)
       response = Net::HTTP.get(URI("#{BASE_URL}#{enc_url}"))
       json_res = JSON.parse(response)
@@ -18,7 +18,7 @@ module Yadisk
       filename = CGI::parse(URI(download_url).query)["filename"][0]
       folder = folder + "/" if not folder.end_with?('/')
 
-      system("wget '#{download_url}' -O '#{folder}#{filename}'")
+      system("wget '#{download_url}' -O '#{folder}#{filename}' #{wget_options}")
     end
   end
 end
