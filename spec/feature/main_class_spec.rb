b78f9d434d50dcf74081c967ac17fef3e64baad5
Artem Yegorov <yegorov0725@yandex.ru>
2020-08-17 16:53:17 +0300

    Add path separator in rspec

--- spec/feature/main_class_spec.rb
+++ spec/feature/main_class_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Yadisk::Main do
     let(:download_file) { |e| Pathname.new(e.metadata[:download_file]) }
     let(:folder) { download_file.dirname.to_s }
 
-    it "must download and save file", download_file: "./spec/files/Обои для рабочего стола.jpg" do
+    it "must download and save file", download_file: File.join(File::SEPARATOR, ".", "spec", "files", "Обои для рабочего стола.jpg") do
       yadisk.download(download_url, folder: folder)
       expect(Pathname.new(download_file)).to be_exist
     end
