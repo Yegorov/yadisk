e8925a6e6db8766c24b5d854fd1aa6c7012a14f4
Artem Yegorov <yegorov0725@yandex.ru>
2020-08-17 16:59:48 +0300

    Fix file separator in Yadisk::Main

--- lib/yadisk.rb
+++ lib/yadisk.rb
@@ -10,13 +10,13 @@ require 'net/http'
 module Yadisk
   class Main
     BASE_URL = 'https://cloud-api.yandex.net:443/v1/disk/public/resources/download?public_key='
-    def download(url, folder: './', wget_options: nil)
+    def download(url, folder: ".#{File::SEPARATOR}", wget_options: nil)
       enc_url = CGI::escape(url)
       response = Net::HTTP.get(URI("#{BASE_URL}#{enc_url}"))
       json_res = JSON.parse(response)
       download_url = json_res['href']
       filename = CGI::parse(URI(download_url).query)["filename"][0]
-      folder = folder + "/" if not folder.end_with?('/')
+      folder = folder + File::SEPARATOR if not folder.end_with?(File::SEPARATOR)
 
       system("wget '#{download_url}' -O '#{folder}#{filename}' #{wget_options}")
     end
